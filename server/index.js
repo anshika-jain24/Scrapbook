4845babe15a4a3827802f014e57d8fbc937680f3
Sai Keshari <saikeshari@gmail.com>
2022-01-08 17:09:42 +0530

    Server cleaned

--- server/index.js
+++ server/index.js
@@ -1,20 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
+
+import connectDB from './config/db.js';
 
 
 const app = express();
-dotenv.config();
 
 app.use(bodyParser.json({limit: "30mb", extented : true}));
 app.use(bodyParser.urlencoded({ limit: "30mb" , extended: true}));
 app.use(cors());
 
-
+connectDB();
 
 const PORT= process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL).then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)));
-
+app.listen(PORT, () => console.log(`server started on port ${PORT}`))
