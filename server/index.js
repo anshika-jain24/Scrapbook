44fd6cb6226b5bed234d04b11c952ad5c4d6f83a
Anshika Jain <jainanshika193@gmail.com>
2022-01-08 16:46:34 +0530

    Cleaned

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import todoRoutes from './routes/todos.js';
+
 
 const app = express();
 dotenv.config();
@@ -12,7 +12,7 @@ app.use(bodyParser.json({limit: "30mb", extented : true}));
 app.use(bodyParser.urlencoded({ limit: "30mb" , extended: true}));
 app.use(cors());
 
-app.use('/todos', todoRoutes);
+
 
 const PORT= process.env.PORT || 5000;
 
